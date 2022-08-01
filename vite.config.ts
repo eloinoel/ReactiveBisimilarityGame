04b3fc16acb757415244dca98599c5a568bf4457
eloinoel <eloi.sandt.games@gmx.de>
2022-08-01 13:18:29 +0200

    what the heck is going wrong

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import replace from '@rollup/plugin-replace';
 
 export default defineConfig({
-  base: '/ReactiveBisimilarityGame/',
+  //base: '/ReactiveBisimilarityGame/',
   build: {
     rollupOptions: {
       plugins: [
