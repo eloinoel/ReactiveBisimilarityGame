ac65363349c649e12f49e95bdc936f1a1f210046
eloinoel <eloi.sandt.games@gmx.de>
2022-10-03 16:41:52 +0200

    no arced transitions needed anymore :)

--- src/scenes/BisimulationLevels/Level2_4.ts
+++ src/scenes/BisimulationLevels/Level2_4.ts
@@ -40,23 +40,26 @@ export default class Level2_4 extends BaseScene {
         game_controller.addState("p3", 0, 1, 1.5);
         game_controller.addState("p4", 0, 2, -1.5);
         game_controller.addState("p5", 0, 2, 1.5);
+        game_controller.addState("p6", 0, 2, 0);
 
         game_controller.addTransition("p0", "p1", "a");
         game_controller.addTransition("p0", "p2", "a");
         game_controller.addTransition("p0", "p3", "a");
         game_controller.addTransition("p1", "p4", "c");
         game_controller.addTransition("p3", "p5", "c");
-        game_controller.addTransition("p1", "p0", "b");
+        game_controller.addTransition("p1", "p6", "b");
         game_controller.addTransition("p2", "p3", "b");
+        game_controller.addTransition("p6", "p5", "c");
 
         game_controller.addState("q0", 1, 0, 0);
         game_controller.addState("q1", 1, 1, 0);
         game_controller.addState("q2", 1, 2, -1);
         game_controller.addState("q3", 1, 2, 1);
+        game_controller.addState("q4", 1, 1, -1.5);
 
 
         game_controller.addTransition("q0", "q1", "a");
-        game_controller.addTransition("q1", "q0", "b");
+        game_controller.addTransition("q1", "q4", "b");
         game_controller.addTransition("q1", "q2", "b");
         game_controller.addTransition("q2", "q3", "c");
         game_controller.addTransition("q1", "q3", "c");
