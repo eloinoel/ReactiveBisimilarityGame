ec1861ae6156ff1877d5cc1e9e128aaa766dec60
eloinoel <eloi.sandt.games@gmx.de>
2022-12-02 12:11:15 +0100

    hide graph print

--- src/scenes/BisimulationLevels/Level2_3.ts
+++ src/scenes/BisimulationLevels/Level2_3.ts
@@ -67,7 +67,7 @@ export default class Level2_3 extends BaseScene {
 
         game_controller.startGame(this, "p0", "q0", false, true, [8, 7]);
         game_controller.printAttackerShortestMinMaxPath()
-        game_controller.printAIGraph()
+        //game_controller.printAIGraph()
         //console.log("expected moves: 7")
     }
 }
